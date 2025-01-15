1813c5e9e9ceb689c100a5dd41b1a047b87e60f6
Gionatan Cernusco <gionatan99@gmail.com>
2025-01-15 18:48:26 +0100

    chore: support only auth with org_key

--- utils/auth.py
+++ utils/auth.py
@@ -13,18 +13,16 @@ def check_auth(token: str = Depends(oauth2_scheme), db: DBClient | None = Depend
     try:
         if db is None: raise HTTPException(status_code=500, detail="Server Error")
         if token is None: raise HTTPException(status_code=401, detail="Unauthorized")
-        is_key = len(token) < 36
-        if is_key: # is a hub key
-            res = (db.table("hub_keys")
+        is_org_key = token.startswith("sk-org-")
+        if is_org_key: # is an org key
+            res = (db.table("org_keys")
                    .select()
                    .eq("value", token)
                    .execute())
             if res is None or len(res.data) == 0: raise HTTPException(status_code=401, detail="Unauthorized")
-            return
+            return res.data[0]["id"]
         else: # is an access_token
-            user = db.auth.get_user(token)
-            if user is None: raise HTTPException(status_code=401, detail="Unauthorized")
-            return
+            raise HTTPException(status_code=401, detail="Unauthorized - You need an organization API key")
     except HTTPException as e:
         raise e
     except AuthApiError as e:
