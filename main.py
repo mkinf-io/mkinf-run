2aa0f900a5f72995aca475226fae2eb5ba2d8a04
Gionatan Cernusco <gionatan99@gmail.com>
2025-02-02 19:55:54 +0100

    chore: setup MCP endpoint

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import traceback
+from dotenv import load_dotenv
+
+load_dotenv()
+
 from typing import Any
 
 import uvicorn
@@ -12,7 +16,7 @@ from utils.db_client import create_db_client, DBClient, get_db
 from utils.repositories import repositories
 from utils.runs import count_run
 from typing import Optional
-from typing import Annotated
+from utils.mcp_client import run_mcp_action
 
 app = FastAPI()
 create_db_client()
@@ -68,6 +72,27 @@ def run_action(owner: str, repo_version: str, action: str, body: str | dict[str,
         print(e)
         raise HTTPException(status_code=500, detail="Server error")
 
+@app.post("/v0.1/mcp/{owner}/{repo_version}/{action}")
+async def run_mcp(owner: str, repo_version: str, action: str, body: dict | None = None,
+               key_id: str = Depends(check_auth), db: Optional[DBClient] = Depends(get_db)):
+    repo, version = repo_version.split(":", 1) if ":" in repo_version else (repo_version, None)
+    # TODO: check for credit card
+    # TODO: Check if can run privates repo (is_private)
+    try:
+        input_tokens = count_tokens(str(body))
+        result = await run_mcp_action(owner=owner, repo=repo, version=version, action=action, arguments=body)
+        output_tokens = count_tokens(str(result))
+        # count_run(db=db, key_id=key_id, owner=owner, repo=repo, action=action, version=version,
+        #           input_tokens=input_tokens, output_tokens=output_tokens)
+        return result
+    except HTTPException as e:
+        raise e
+    except HTTPError as e:
+        raise HTTPException(status_code=e.response.status_code, detail=e.response.text)
+    except Exception as e:
+        traceback.print_exc()
+        print(e)
+        raise HTTPException(status_code=500, detail="Server error")
 
 # Additional route
 @app.get("/about")
