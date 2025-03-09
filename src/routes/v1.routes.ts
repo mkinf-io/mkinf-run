1974cdbbf62246531d4dbe73143dc75585bccb4f
Gionatan Cernusco <gionatan99@gmail.com>
2025-03-09 17:30:21 +0100

    chore: removes session related stuffs

--- src/routes/v1.routes.ts
+++ src/routes/v1.routes.ts
@@ -1,17 +1,8 @@
 import express from 'express';
-import { closeRunSession, listTools, listToolsOnce, runAction, runActionOnce, startRunSession } from '../controllers/v1/runController';
+import { listToolsOnce, runActionOnce } from '../controllers/v1/runController';
 
 const v1Router = express.Router({ mergeParams: true });
 
-// Session List Tools
-v1Router.get('/sessions/:sessionId', listTools);
-// Session Run Action
-v1Router.post('/sessions/:sessionId/:action', runAction);
-// Close Session
-v1Router.delete('/sessions/:sessionId', closeRunSession);
-// Start Session
-v1Router.post('/:owner/:repo', startRunSession);
-v1Router.post('/:owner/:repo@:version', startRunSession);
 // List Tools Once
 v1Router.get('/:owner/:repo', listToolsOnce);
 // Run Action Once
